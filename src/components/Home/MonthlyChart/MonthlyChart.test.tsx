b63c4f803760038e20a1a7ee14eade2d11ab8a2a
WATA-Yiju <yiju.dev@watanow.com>
2024-07-02 15:10:15 +0900

    feat(test): add mocking recharts

--- src/components/Home/MonthlyChart/MonthlyChart.test.tsx
+++ src/components/Home/MonthlyChart/MonthlyChart.test.tsx
@@ -4,6 +4,21 @@ import { render } from '@/utils/test-helpers';
 import { screen } from '@testing-library/dom';
 import MonthlyChart from '.';
 
+// Mocking recharts
+vi.mock('recharts', () => ({
+  // 여기서 필요한 recharts 모듈의 컴포넌트와 함수들을 mock합니다.
+  Bar: vi.fn(),
+  Cell: vi.fn(),
+  LabelList: vi.fn(),
+  Legend: vi.fn(),
+  BarChart: vi.fn(),
+  Rectangle: vi.fn(),
+  ResponsiveContainer: vi.fn(),
+  Tooltip: vi.fn(),
+  XAxis: vi.fn(),
+  // 필요한 다른 recharts 컴포넌트와 함수들을 추가로 mock 합니다.
+}));
+
 const { assetResponse, prevAssetResponse } = fixtures;
 const assetList = convertAssetResponse(assetResponse);
 const prevAssetList = convertAssetResponse(prevAssetResponse);
