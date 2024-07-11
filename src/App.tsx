831a7ca892985aa9b8270f2c1e1f08e6bf475442
WATA-Yiju <yiju.dev@watanow.com>
2024-07-11 12:07:31 +0900

    fix(app): remove useCleanHash

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
-import useCleanHash from './hooks/useCleanHash';
 import useSession from './hooks/useSession';
 import routes from './routes';
 import defaultTheme from './styles/defaultTheme';
@@ -12,7 +11,7 @@ const router = createBrowserRouter(routes);
 function App() {
   const theme = defaultTheme;
 
-  useCleanHash();
+  // useCleanHash();
 
   useSession();
 
